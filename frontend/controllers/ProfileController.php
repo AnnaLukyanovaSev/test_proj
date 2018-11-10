6456db12e128e3a758883991a664d3cb627e92a4
Lukyanova Anna <annieromanenko@gmail.com>
2018-11-10 11:34:41 +0300

    frC profile

---
+++ frontend/controllers/ProfileController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\Profile;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use common\models\forms\ProfileForm;
+use common\models\User;
+use yii\filters\AccessControl;
+
+class ProfileController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['info', 'error', 'index','update'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    public function actionInfo()
+    {
+        $model = Profile::find()->select("first_name, last_name")
+            ->where('user_id=:user_id', [':user_id' => strval(Yii::$app->user->identity->getId())])->one();
+        if ($model) {
+            return $this->render('index', ['model' => $model]);
+        }
+
+        $model = new Profile();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['index', 'id' => $model->user_id]);
+        }
+
+        return $this->render('info', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionIndex()
+    {
+         $us =Yii::$app->user->identity->getId() ;
+        $model = Profile::find()->select("first_name, last_name")
+            ->where('user_id=:user_id', [':user_id' => $us])->one();
+        return $this->render('index', ['model' => $model]);
+    }
+
+    public function actionUpdate()
+    {
+        $us =Yii::$app->user->identity->getId() ;
+        $model = Profile::findOne($us);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['index', 'id' => $model->user_id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+}
+
